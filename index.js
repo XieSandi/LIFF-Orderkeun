b52b3d6aa235195b7149c28e7b7e9fa14330a8d8
Sandi Pratama <yue_xie@outlook.com>
2020-12-28 17:26:18 +0700

    up

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+
+//LIFF
 const myLiffId = process.env.MY_LIFF_ID;
+
+//Line BOT
+const config = {
+    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+    channelSecret: process.env.CHANNEL_SECRET,
+  };
+const client = new line.Client(config);
 
 app.use(express.static('public'));
 
@@ -10,3 +19,35 @@ app.get('/send-id', function(req, res) {
 });
 
 app.listen(port, () => console.log(`app listening on port ${port}!`));
+
+// register a webhook handler with middleware
+// about the middleware, please refer to doc
+app.post('/callback', line.middleware(config), (req, res) => {
+    Promise
+      .all(req.body.events.map(handleEvent))
+      .then((result) => res.json(result))
+      .catch((err) => {
+        console.error(err);
+        res.status(500).end();
+      });
+  });
+  
+  // event handler
+  function handleEvent(event) {
+    if (event.type !== 'message' || event.message.type !== 'text') {
+      // ignore non-text-message event
+      return Promise.resolve(null);
+    }
+  
+    // create a echoing text message
+    const echo = { type: 'text', text: event.message.text };
+  
+    // use reply API
+    return client.replyMessage(event.replyToken, echo);
+  }
+  
+  // listen on port
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => {
+    console.log(`listening on ${port}`);
+  });
