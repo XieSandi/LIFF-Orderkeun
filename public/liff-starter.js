ceb17068cc6da84a7e0e96e083eb0aeebca5833b
Sandi Pratama <yue_xie@outlook.com>
2020-12-28 19:04:03 +0700

    Update liff-starter.js

--- public/liff-starter.js
+++ public/liff-starter.js
@@ -96,7 +96,7 @@ function pesan(paket,harga){
       if (showConfirm == true){
         liff.sendMessages([{
             'type': 'text',
-            'text': "Hallo ! Aku pesen " + paket
+            'text': "Hallo! Aku pesen " + paket
         }]).then(function() {
             window.alert('Message sent');
         }).catch(function(error) {
