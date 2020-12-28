cbbc87776ac37aed21005c416a32e7d1941f2565
Sandi Pratama <yue_xie@outlook.com>
2020-12-28 11:21:55 +0700

    up

--- public/liff-starter.js
+++ public/liff-starter.js
@@ -138,15 +138,15 @@ function registerButtonHandlers() {
     });
 
     // get access token
-    document.getElementById('getAccessToken').addEventListener('click', function() {
-        if (!liff.isLoggedIn() && !liff.isInClient()) {
-            alert('To get an access token, you need to be logged in. Please tap the "login" button below and try again.');
-        } else {
-            const accessToken = liff.getAccessToken();
-            document.getElementById('accessTokenField').textContent = accessToken;
-            toggleAccessToken();
-        }
-    });
+    // document.getElementById('getAccessToken').addEventListener('click', function() {
+    //     if (!liff.isLoggedIn() && !liff.isInClient()) {
+    //         alert('To get an access token, you need to be logged in. Please tap the "login" button below and try again.');
+    //     } else {
+    //         const accessToken = liff.getAccessToken();
+    //         document.getElementById('accessTokenField').textContent = accessToken;
+    //         toggleAccessToken();
+    //     }
+    // });
 
     // login call, only when external browser is used
     document.getElementById('liffLoginButton').addEventListener('click', function() {
@@ -175,23 +175,23 @@ function sendAlertIfNotInClient() {
 /**
 * Toggle access token data field
 */
-function toggleAccessToken() {
-    toggleElement('accessTokenData');
-}
+// function toggleAccessToken() {
+//     toggleElement('accessTokenData');
+// }
 
 /**
 * Toggle profile info field
 */
-function toggleProfileData() {
-    toggleElement('profileInfo');
-}
+// function toggleProfileData() {
+//     toggleElement('profileInfo');
+// }
 
 /**
 * Toggle scanCode result field
 */
-function toggleQrCodeReader() {
-    toggleElement('scanQr');
-}
+// function toggleQrCodeReader() {
+//     toggleElement('scanQr');
+// }
 
 /**
 * Toggle specified element
