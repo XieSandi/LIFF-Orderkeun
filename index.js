ebee752296ab119d5f5dba981b4e784c98215fc0
Sandi Pratama <yue_xie@outlook.com>
2020-12-29 23:34:22 +0700

    Update index.js

--- index.js
+++ index.js
@@ -26,16 +26,8 @@ app.post('/callback', line.middleware(config), (req, res) => {
   
   // event handler
   function handleEvent(event) {
-    // if (event.type !== 'message' || event.message.type !== 'text' || event.message.text == 'Pesan') {
-    //   // ignore non-text-message event
-    //   return Promise.resolve(null);
-    // }
-  
-    // create a echoing text message
-    // const echo = { type: 'text', text: event.message.text };
-  
-    // use reply API
-    if (event.type == 'message' || event.message.type == 'text' || event.message.text == 'Pesan') {
+    // event.type == 'message' || event.message.type == 'text' || 
+    if (event.type == 'message' && event.message.type == 'text' && event.message.text == 'Pesan') {
         return client.replyMessage
             (event.replyToken,
                 {
