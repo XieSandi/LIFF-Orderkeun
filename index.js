557e6c17ef6d7169a6b4d542f57c7ce87bbd8112
Sandi Pratama <yue_xie@outlook.com>
2020-12-29 22:23:31 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
+'use strict';
+
+const line = require('@line/bot-sdk');
 const express = require('express');
-const app = express();
 const port = process.env.PORT || 5000;
 const myLiffId = process.env.MY_LIFF_ID;
+
+const config = {
+    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+    channelSecret: process.env.CHANNEL_SECRET,
+  };
+
+const client = new line.Client(config);
+
+const app = express();
+
+app.post('/callback', line.middleware(config), (req, res) => {
+    Promise
+      .all(req.body.events.map(handleEvent))
+      .then((result) => res.json(result))
+      .catch((err) => {
+        console.error(err);
+        res.status(500).end();
+      });
+  });
+  
+  // event handler
+  function handleEvent(event) {
+    if (event.type !== 'message' || event.message.type !== 'text') {
+      // ignore non-text-message event
+      return Promise.resolve(null);
+    }
+  
+    // create a echoing text message
+    const echo = { type: 'text', text: event.message.text };
+  
+    // use reply API
+    return client.replyMessage(event.replyToken, echo);
+  }  
 
 app.use(express.static('public'));
 
