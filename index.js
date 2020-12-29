752c44a89a9dbdb62b034d5489f829d08ad9972b
Sandi Pratama <yue_xie@outlook.com>
2020-12-29 22:32:04 +0700

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.post('/callback', line.middleware(config), (req, res) => {
     }
   
     // create a echoing text message
-    const echo = { type: 'text', text: event.message.text };
+    // const echo = { type: 'text', text: event.message.text };
   
     // use reply API
     return client.replyMessage(event.replyToken, echo);
