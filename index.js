0a7cb44fa877b5e6871a0c2bd66298f47befe33c
Sandi Pratama <yue_xie@outlook.com>
2020-12-30 13:29:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -26,20 +26,20 @@ app.post('/callback', line.middleware(config), (req, res) => {
       });
   });
 
-client.getProfile('<userId>')
-  .then((profile) => {
-    console.log(profile.displayName);
-    console.log(profile.userId);
-    console.log(profile.pictureUrl);
-    console.log(profile.statusMessage);
-  })
-  .catch((err) => {
-    // error handling
-  })
+// client.getProfile('<userId>')
+//   .then((profile) => {
+//     console.log(profile.displayName);
+//     console.log(profile.userId);
+//     console.log(profile.pictureUrl);
+//     console.log(profile.statusMessage);
+//   })
+//   .catch((err) => {
+//     // error handling
+//   })
   
   // event handler
   function handleEvent(event) {
-      
+
     client.getProfile('<userId>')
     .then((profile) => {
       console.log(profile.displayName);
