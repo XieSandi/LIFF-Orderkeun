5c340f92af5b35f5f09ef274fc52e117a1a90105
Sandi Pratama <yue_xie@outlook.com>
2020-12-30 15:50:42 +0700

    Update index.js

--- index.js
+++ index.js
@@ -159,7 +159,7 @@ client.getProfile('<userId>')
                             "contents": [
                                 {
                                     "type": "image",
-                                    "url": "https://images.unsplash.com/photo-1602192103201-d763907bc41b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+                                    "url": "img/2.jfif"
                                 },
                                 {
                                     "type": "text",
